3095063234bef5fe1627c88ef48998eabe16874b
DavidNemeth <david.nemeth90@gmail.com>
2016-03-01 04:00:38 +0100

    Base

---
+++ ShopAssistant/ShopAssistant/Base/ContainerHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Unity;
+using ShopAssistant.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopAssistant.Base
+{
+    public static class ContainerHelper
+    {
+        private static IUnityContainer _container;
+        static ContainerHelper()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<INapiAdatokRepository, NapiAdatokRepository>(
+                new ContainerControlledLifetimeManager());
+        }
+        public static IUnityContainer Container
+        {
+            get { return _container; }
+        }
+    }
+}
