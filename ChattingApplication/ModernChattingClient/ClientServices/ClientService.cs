6321d9d31b1df0a88f6ed9afa9d5563f1aa1e130
DavidNemeth <david.nemeth90@gmail.com>
2016-08-20 08:06:59 +0200

    Ui fix

---
+++ ChattingApplication/ModernChattingClient/ClientServices/ClientService.cs
@@ -0,0 +1,32 @@
+﻿using ChattingInterfaces;
+using ModernChattingClient.Pages.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernChattingClient.ClientServices
+{
+    [CallbackBehavior(ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class ClientService : IClientService
+    {
+        public void GetMessage(string message, string userName)
+        {
+            ChatViewModel.GetInstance().TakeMessage(message, userName);
+        }
+
+        public void Update(bool value, string userName)
+        {
+            if (value)
+            {
+                ChatViewModel.GetInstance().Users.Add(userName);
+            }
+            else
+            {
+                ChatViewModel.GetInstance().Users.Remove(userName);
+            }
+        }
+    }
+}
---
+++ ChattingApplication/ModernChattingClient/Pages/Chat/ChatViewModel.cs
@@ -0,0 +1,61 @@
+﻿using ModernChattingClient.Base;
+using System.Collections.ObjectModel;
+
+namespace ModernChattingClient.Pages.Chat
+{
+    public class ChatViewModel : BindableBase
+    {
+        public static ChatViewModel _this;
+
+        public ChatViewModel()
+        {
+            _this = this;
+        }
+
+        #region props
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set { SetProperty(ref userName, value); }
+        }
+
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                SetProperty(ref message, value);
+                OnPropertyChanged("SendEnabled");
+            }
+        }
+
+        private string chat;
+        public string Chat
+        {
+            get { return chat; }
+            set { SetProperty(ref chat, value); }
+        }
+
+        private ObservableCollection<string> users = new ObservableCollection<string>();
+        public ObservableCollection<string> Users
+        {
+            get { return users; }
+            set { SetProperty(ref users, value); }
+        }
+        public string Pw { get; set; }
+        #endregion
+
+
+        public static ChatViewModel GetInstance()
+        {
+            return _this;
+        }
+
+        public void TakeMessage(string message, string username)
+        {
+            Chat += username + ": " + message + "\n";
+        }
+    }
+}
